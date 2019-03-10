0ac7227eaa33b7834de8e8636b53e8db76c92f65
Oleg <oleg@relikd.de>
2019-03-10 12:16:32 +0100

    Fix: 'Update all feeds' unread count during update

--- baRSS/Status Bar Menu/BarMenu.m
+++ baRSS/Status Bar Menu/BarMenu.m
@@ -154,6 +154,7 @@
 		[self.unreadMap updateAllCounts:updated forPath:feed.indexPath];
 		// 2. rebuild articles menu if it is open
 		if (item.submenu.isFeedMenu) { // menu item is visible
+			item.image = [feed iconImage16];
 			item.enabled = (feed.articles.count > 0);
 			if (item.submenu.numberOfItems > 0) { // replace articles menu
 				[item.submenu removeAllItems];
