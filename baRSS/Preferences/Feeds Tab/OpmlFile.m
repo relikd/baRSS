21e2d6706fd60a6b17a7b7fc5d0cc7d82fad9a7b
relikd <info@relikd.de>
2019-07-25 18:56:08 +0200

    File rename

--- baRSS/Preferences/Feeds Tab/OpmlFile.m
+++ baRSS/Preferences/Feeds Tab/OpmlFile.m
@@ -20,7 +20,7 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //  SOFTWARE.
 
-#import "OpmlExport.h"
+#import "OpmlFile.h"
 #import "FeedMeta+Ext.h"
 #import "FeedGroup+Ext.h"
 #import "StoreCoordinator.h"
