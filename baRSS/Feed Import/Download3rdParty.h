dbcb4ca6c36ad6d12f604993883fed5079381633
Oleg <oleg@relikd.de>
2020-12-18 10:37:36 +0100

    Support for yt /c/channel-name URLs

--- baRSS/Feed Import/Download3rdParty.h
+++ baRSS/Feed Import/Download3rdParty.h
@@ -28,7 +28,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 // TODO: Make plugins extensible? community extensions.
 @interface YouTubePlugin : NSObject
-+ (NSString*)feedURL:(NSURL*)url;
++ (NSString*)feedURL:(NSURL*)url data:(NSData*)html;
 + (NSString*)videoImage:(NSString*)videoid;
 + (NSString*)videoImageHQ:(NSString*)videoid;
 @end
