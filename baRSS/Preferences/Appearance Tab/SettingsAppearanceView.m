7a805ccdc45c693a2e3f86a3df67cde817c2fc7c
relikd <info@relikd.de>
2025-12-02 18:42:39 +0100

    feat: show tooltip for all appearance settings

--- baRSS/Preferences/Appearance Tab/SettingsAppearanceView.m
+++ baRSS/Preferences/Appearance Tab/SettingsAppearanceView.m
@@ -23,18 +23,45 @@
 	ColumnIcon(self, __X, RSSImageSettingsFeed, NSLocalizedString(@"Show in feed menu", nil));
 	// Generate checkbox matrix
 	self.y = PAD_WIN + IconSize + PAD_S;
-	[self entry:NSLocalizedString(@"Tint menu bar icon on unread", nil) c1:Pref_globalTintMenuIcon c2:nil c3:nil];
-	[self entry:NSLocalizedString(@"Update all feeds", nil) c1:Pref_globalUpdateAll c2:nil c3:nil];
-	[self entry:NSLocalizedString(@"Open all unread", nil) c1:Pref_globalOpenUnread c2:Pref_groupOpenUnread c3:Pref_feedOpenUnread];
-	[self entry:NSLocalizedString(@"Mark all read", nil) c1:Pref_globalMarkRead c2:Pref_groupMarkRead c3:Pref_feedMarkRead];
-	[self entry:NSLocalizedString(@"Mark all unread", nil) c1:Pref_globalMarkUnread c2:Pref_groupMarkUnread c3:Pref_feedMarkUnread];
-	[self entry:NSLocalizedString(@"Number of unread articles", nil) c1:Pref_globalUnreadCount c2:Pref_groupUnreadCount c3:Pref_feedUnreadCount];
-	[self entry:NSLocalizedString(@"Indicator for unread articles", nil) c1:nil c2:Pref_groupUnreadIndicator c3:Pref_feedUnreadIndicator];
-	[self entry:NSLocalizedString(@"Show only unread / hide read", nil) c1:nil c2:Pref_groupUnreadOnly c3:Pref_feedUnreadOnly];
-	[[self entry:NSLocalizedString(@"Truncate article title", nil) c1:nil c2:nil c3:Pref_feedTruncateTitle]
-	 tooltip:NSLocalizedString(@"Truncate article title after 60 characters", nil)];
-	[[self entry:NSLocalizedString(@"Limit number of articles", nil) c1:nil c2:nil c3:Pref_feedLimitArticles]
-	 tooltip:NSLocalizedString(@"Display at most 40 articles in feed menu", nil)];
+	[[self entry:NSLocalizedString(@"Tint menu bar icon on unread", nil)
+			  c1:Pref_globalTintMenuIcon c2:nil c3:nil]
+	 tooltip:NSLocalizedString(@"If active, a color will indicate if there are unread articles.", nil)];
+	
+	[[self entry:NSLocalizedString(@"Update all feeds", nil)
+			  c1:Pref_globalUpdateAll c2:nil c3:nil]
+	 tooltip:NSLocalizedString(@"Show a button in status bar menu to reload all feeds. This will force fetch new online content regardless of next-update timer.", nil)];
+	
+	[[self entry:NSLocalizedString(@"Open all unread", nil)
+			  c1:Pref_globalOpenUnread c2:Pref_groupOpenUnread c3:Pref_feedOpenUnread]
+	 tooltip:NSLocalizedString(@"Show a button to open unread articles. (globally / per group / per feed)\n\nIf you hold down option key, this will become an “open a few” unread articles button.", nil)];
+	
+	[[self entry:NSLocalizedString(@"Mark all read", nil)
+			  c1:Pref_globalMarkRead c2:Pref_groupMarkRead c3:Pref_feedMarkRead]
+	 tooltip:NSLocalizedString(@"Show a button to mark articles read. (globally / per group / per feed)", nil)];
+	
+	[[self entry:NSLocalizedString(@"Mark all unread", nil)
+			  c1:Pref_globalMarkUnread c2:Pref_groupMarkUnread c3:Pref_feedMarkUnread]
+	 tooltip:NSLocalizedString(@"Show a button to mark articles unread. (globally / per group / per feed)\n\nYou can hold down option key and click on an article to toggle that item (un-)read.", nil)];
+	
+	[[self entry:NSLocalizedString(@"Number of unread articles", nil)
+			  c1:Pref_globalUnreadCount c2:Pref_groupUnreadCount c3:Pref_feedUnreadCount]
+	 tooltip:NSLocalizedString(@"Show count of unread articles in parenthesis. (on menu bar icon / on group folder / on feed folder)", nil)];
+	
+	[[self entry:NSLocalizedString(@"Indicator for unread articles", nil)
+			  c1:nil c2:Pref_groupUnreadIndicator c3:Pref_feedUnreadIndicator]
+	 tooltip:NSLocalizedString(@"Show blue dot on menu items with unread articles. (on group & feed folder / on article entry)", nil)];
+	
+	[[self entry:NSLocalizedString(@"Show only unread", nil)
+			  c1:nil c2:Pref_groupUnreadOnly c3:Pref_feedUnreadOnly]
+	 tooltip:NSLocalizedString(@"Hide articles which have been read. (hide group & feed folders / hide articles inside of feed folder)", nil)];
+	
+	[[self entry:NSLocalizedString(@"Truncate article title", nil)
+			  c1:nil c2:nil c3:Pref_feedTruncateTitle]
+	 tooltip:NSLocalizedString(@"Truncate article title after 60 characters. If a title is longer than that, show an ellipsis character “…” instead.", nil)];
+	
+	[[self entry:NSLocalizedString(@"Limit number of articles", nil)
+			  c1:nil c2:nil c3:Pref_feedLimitArticles]
+	 tooltip:NSLocalizedString(@"Display at most 40 articles in feed menu. Remaining articles will be hidden from view but are still there. Unread count may be confusing as it will also count unread and hidden articles.", nil)];
 	return self;
 }
 
